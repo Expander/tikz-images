c524a0f1b057da71d3f63c46195bdf4f07c5f632
Alexander Voigt <ahgvoigt@gmail.com>
2020-09-16 10:03:32 +0200

    execute all scripts

--- create-all.sh
+++ create-all.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-dirs="
-lens-diacaustic
-lens-paraxial
-"
+script='create.sh'
+basedir=$(dirname $0)
+dirs=$(find ${basedir} -name ${script} -type f | xargs dirname)
 
 for f in ${dirs}
 do
-    (cd $f; [ -e create.sh ] && ./create.sh)
+    (cd $f; [ -e ${script} ] && ./${script})
 done
